ed03f4ce3952de3088f2036f275db3d89b7540e2
Gordon Smith <gordonjsmith@gmail.com>
2016-05-12 14:05:55 -0400

    WIP
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

---
+++ test/templatesFactory.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define([], factory);
+    } else {
+        root.test_templatesFactory = factory();
+    }
+}(this, function (DataFactory, HeatMap, WordCloud, Table) {
+    return {
+        HTML: {
+            simple: function (callback) {
+                require(["test/DataFactory", "templates/HTML"], function (DataFactory, HTML) {
+                    callback(new HeatMap()
+                        .columns(DataFactory.HeatMap.simple.columns)
+                        .data(DataFactory.HeatMap.simple.data)
+                    );
+                });
+            }
+        },
+        SVG: {
+            simple: function (callback) {
+                require(["test/DataFactory", "templates/SVG"], function (DataFactory, SVG) {
+                    callback(new HeatMap()
+                        .columns(DataFactory.HeatMap.simple.columns)
+                        .data(DataFactory.HeatMap.simple.data)
+                    );
+                });
+            }
+        }
+
+    };
+}));
