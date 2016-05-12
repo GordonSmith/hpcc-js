75d30eaa426693178136c26aa051ed8329dbf55a
Gordon Smith <gordonjsmith@gmail.com>
2016-05-12 09:16:01 -0400

    WIP
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

---
+++ test/templateFactory.js
@@ -0,0 +1,31 @@
+﻿"use strict";
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define([], factory);
+    } else {
+        root.test_otherFactory = factory();
+    }
+}(this, function () {
+    return {
+        SVG: {
+            simple: function (callback) {
+                require(["test/DataFactory", "templates/SVGTemplate"], function (DataFactory, SVGTemplate) {
+                    callback(new SVGTemplate()
+                        .columns(DataFactory.ND.subjects.columns)
+                        .data(DataFactory.ND.subjects.data)
+                    );
+                });
+            }
+        },
+        HTML: {
+            simple: function (callback) {
+                require(["test/DataFactory", "templates/HTMLTemplate"], function (DataFactory, SVGTemplate) {
+                    callback(new SVGTemplate()
+                        .columns(DataFactory.ND.subjects.columns)
+                        .data(DataFactory.ND.subjects.data)
+                    );
+                });
+            }
+        },
+    };
+}));
