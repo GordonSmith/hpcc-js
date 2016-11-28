7a1564d8fd8dd3b7a82392fb20be93dbdebe8c04
Gordon Smith <gordonjsmith@gmail.com>
2016-11-28 18:31:34 +0000

    Switch to ES6 import syntax.
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- src/c3chart/Pie.js
+++ src/c3chart/Pie.js
@@ -1,35 +1,26 @@
-"use strict";
-(function (root, factory) {
-    if (typeof define === "function" && define.amd) {
-        define(["./Common2D"], factory);
-    } else {
-        root.c3chart_Pie = factory(root.c3chart_Common2D);
-    }
-}(this, function (Common2D) {
-    function Pie(target) {
-        Common2D.call(this);
+import { Common2D } from "./Common2D";
 
-        this._type = "pie";
-    }
-    Pie.prototype = Object.create(Common2D.prototype);
-    Pie.prototype.constructor = Pie;
-    Pie.prototype._class += " c3chart_Pie";
+export function Pie(target) {
+    Common2D.call(this);
 
-    Pie.prototype.update = function (domNode, element) {
-        Common2D.prototype.update.apply(this, arguments);
-    };
+    this._type = "pie";
+}
+Pie.prototype = Object.create(Common2D.prototype);
+Pie.prototype.constructor = Pie;
+Pie.prototype._class += " c3chart_Pie";
 
-    Pie.prototype.getChartOptions = function () {
-        var chartOptions = Common2D.prototype.getChartOptions.apply(this, arguments);
+Pie.prototype.update = function (domNode, element) {
+    Common2D.prototype.update.apply(this, arguments);
+};
 
-        var data = this.data().map(function (row, idx) {
-            return [row[0], row[1]];
-        }, this);
+Pie.prototype.getChartOptions = function () {
+    var chartOptions = Common2D.prototype.getChartOptions.apply(this, arguments);
 
-        chartOptions.columns = data;
+    var data = this.data().map(function (row, idx) {
+        return [row[0], row[1]];
+    }, this);
 
-        return chartOptions;
-    };
+    chartOptions.columns = data;
 
-    return Pie;
-}));
+    return chartOptions;
+};
