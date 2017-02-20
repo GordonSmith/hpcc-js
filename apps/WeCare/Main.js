8b4efdce6f57a1dabfabbafc877830cd7ab87d7a
Gordon Smith <gordonjsmith@gmail.com>
2017-02-20 05:13:29 +0000

    WIP
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- apps/WeCare/Main.js
+++ apps/WeCare/Main.js
@@ -300,7 +300,10 @@
                 newRequest.agelow = ages[0];
                 newRequest.agehigh = ages[1];
                 newRequest.demomode = demomode;
-
+                var tmp = JSON.stringify(newRequest);
+                newRequest = {
+                    "Date1": "19900613", "Address1": "1740 Maryland Ave", "City1": "Charlotte", "State1": "NC", "Zip1": "28209", "Zip1LowYYYYMM": "199002", "Zip1HighYYYYMM": "199101", "Date2": "19910829", "Address2": "1512 Turnstone Ct", "City2": "Rock Hill", "State2": "SC", "Zip2": "29732", "Zip2LowYYYYMM": "199104", "Zip2HighYYYYMM": "199204", "Date3": "19990109", "Address3": "6147 Garamond Ct", "City3": "Charlotte", "State3": "NC", "Zip3": "28270", "Zip3LowYYYYMM": "199809", "Zip3HighYYYYMM": "199908", "GeoDistanceThreshold": "5", "demomode": true
+                };
                 context.connWeCare.jsonp(newRequest).then(function (response) {
                     var results = response["wecares.serialoffenderfinderserviceResponse"].Results.TroySearch.Row;
                     context.loadDebugTable(results)
