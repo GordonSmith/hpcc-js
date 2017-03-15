9ef1a9f83a688612cdefe82fdf362a1451129a86
Gordon Smith <gordonjsmith@gmail.com>
2017-03-15 17:19:06 +0000

    v4 Sankey
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+//  Common  ---
 export * from "./common/Icon";
 export * from "./common/Image";
 export * from "./common/Text";
 export * from "./common/TextBox";
 
+//  Chart  ---
 export * from "./chart/Column";
 export * from "./chart/Bar";
 export * from "./chart/Pie";
@@ -14,8 +16,12 @@ export * from "./chart/Step";
 export * from "./chart/Gantt";
 export * from "./chart/HexBin";
 
+//  Tree  ---
 export { Column as TreemapColumn, Treemap } from "./tree/Treemap";
 export * from "./tree/CirclePacking";
 export * from "./tree/SunburstPartition";
 export { Column as IndentedColumn, Indented } from "./tree/Indented";
 export { Column as DendrogramColumn, Dendrogram } from "./tree/Dendrogram";
+
+//  Graph  ---
+export { Column as SankeyColumn, Sankey } from "./graph/Sankey";
