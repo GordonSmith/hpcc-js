07ec38c85781bf867036467902cc48df605f8dc9
Gordon Smith <gordonjsmith@gmail.com>
2017-05-03 16:46:26 +0100

    WIP Switch to preact
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,24 @@
-import { select as d3Select } from "d3-selection";
+// import { select as d3Select } from "d3-selection";
+import { Component, h, render } from "preact";
 import { Test } from "../src/html/test";
 
 const test = new Test()
     .target("placeholder")
+    .columns(["label", "weight"])
+    .data([
+        ["Test", 200],
+        ["Test2", 100]
+    ])
     .render()
     ;
+
+let idx = 0;
+setInterval(() => {
+    test.title = "Hello" + ++idx;
+    test
+        .data([
+            ["Test", 200 * Math.random()],
+            ["Test2", 100 * Math.random()]
+        ])
+        .render();
+}, 1000000);
