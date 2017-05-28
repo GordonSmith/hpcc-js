958cbf0dcb7c25229342e0d4ae009f7764fed585
Gordon Smith <gordonjsmith@gmail.com>
2017-05-28 13:54:24 +0100

    Comms/GH-3 Redirect Logging
    Signed-off-by: Gordon Smith gordonjsmith@gmail.com

--- tests/test-comms/src/index.spec.ts
+++ tests/test-comms/src/index.spec.ts
@@ -1,3 +1,7 @@
 import 'es6-promise/auto';  // polyfill Promise on IE
 
+import './services/wsDFU.spec';
+import './services/wsTopology.spec';
+import './services/wsWorkunits.spec';
 import './connection.spec';
+import './espConnection.spec';
