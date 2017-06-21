f4d8c42ae7e48da5450a4b5af136fdcb035af4c7
Gordon Smith <gordonjsmith@gmail.com>
2017-06-21 15:46:39 +0100

    Start of optimized bundling
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

---
+++ packages/loader/src/meta.ts
@@ -0,0 +1,6 @@
+export const shims = ["codemirror-shim", "c3-shim", "dgrid-shim", "phosphor-shim", "preact-shim"];
+export const packages = [
+    "common", "layout", "phosphor", "api", "dgrid", "chart", "other", "form",
+    "c3chart", "google", "amchart", "tree", "graph", "map",
+    "handson", "react", "composite", "marshaller", "ddl", "html", "codemirror"
+];
