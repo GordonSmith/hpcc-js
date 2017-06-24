c9c2563f61bc25d156d138158ce06857f2786dfd
Gordon Smith <gordonjsmith@gmail.com>
2017-06-24 11:40:42 +0100

    WIP
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- packages/loader/src/meta.ts
+++ packages/loader/src/meta.ts
@@ -50,7 +50,7 @@ export const thirdParty = {
 };
 export const shims = ["loader", "codemirror-shim", "c3-shim", "dgrid-shim", "phosphor-shim", "preact-shim"];
 export const packages = [
-    "common", "layout", "phosphor", "api", "dgrid", "chart", "other", "form",
+    "comms", "util", "common", "layout", "phosphor", "api", "dgrid", "chart", "other", "form",
     "c3chart", "google", "amchart", "tree", "graph", "map",
     "handson", "react", "composite", "marshaller", "ddl", "html", "codemirror"
 ];
