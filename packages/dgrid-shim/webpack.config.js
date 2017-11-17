d8191a7198bf6143b6213a6650c51970e7bf83a2
Gordon Smith <gordonjsmith@gmail.com>
2017-11-17 12:05:08 +0000

    Fix dgrid-shim
    Signed-off-by: Gordon Smith <gordonjsmith@gmail.com>

--- packages/dgrid-shim/webpack.config.js
+++ packages/dgrid-shim/webpack.config.js
@@ -8,24 +8,36 @@ module.exports = {
     entry: "./lib/index.js",
     output: {
         path: path.join(__dirname, "build"),
-        filename: "dgrid-shim.js",
+        publicPath: "build/",
+        pathinfo: true,
         libraryTarget: "umd",
-        library: "@hpcc-js/dgrid-shim"
+        library: "@hpcc-js/dgrid-shim",
+        filename: "dgrid-shim.js"
     },
     module: {
         loaders: [
-            { test: /\.(png)|(gif)$/, loader: "url-loader?limit=100000" }
-        ]
+            {
+                test: /\.(png)|(gif)$/, loader: "url-loader?limit=100000"
+            }, {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }, {
+                test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: './build/[name].[ext]'
+                    }
+                }]
+            }]
     },
     plugins: [
         new DojoWebpackPlugin({
             loaderConfig: require.resolve("./src/loaderConfig"),
             environment: { dojoRoot: "release" },	// used at run time for non-packed resources (e.g. blank.gif)
             buildEnvironment: { dojoRoot: "../../node_modules" }, // used at build time
-            coerceUndefinedToFalse: true,
             locales: ["en"]
         }),
-
         // For plugins registered after the DojoAMDPlugin, data.request has been normalized and
         // resolved to an absMid and loader-config maps and aliases have been applied
         new webpack.NormalModuleReplacementPlugin(/^dojox\/gfx\/renderer!/, "dojox/gfx/canvas"),
@@ -33,16 +45,17 @@ module.exports = {
             /^css!/, function (data) {
                 data.request = data.request.replace(/^css!/, "!style-loader!css-loader!")
             }
-        )/*,
+        ),
         new webpack.optimize.UglifyJsPlugin({
             output: { comments: false },
             compress: { warnings: false },
             sourceMap: true
         })
-        */
     ],
     resolveLoader: {
-        modules: ["../../node_modules"]
+        modules: [
+            path.join(__dirname, "../../node_modules")
+        ]
     },
     devtool: "#source-map",
     node: {
--- packages/dgrid/rollup.config.js
+++ packages/dgrid/rollup.config.js
@@ -4,7 +4,7 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import postcss from "rollup-plugin-postcss";
 const definition = require("./package.json");
 const name = definition.name.split("/").pop();
-const external = Object.keys(definition.dependencies || {}).filter(dep => dep.indexOf("@hpcc-js") === 0);  //  Rollup dies if we include dgrid-shim as static lib && dep.indexOf("-shim") < 0);
+const external = Object.keys(definition.dependencies || {}).filter(dep => dep.indexOf("@hpcc-js") === 0 && dep.indexOf("-xxxxshim") < 0);
 const globals = {};
 external.forEach(dep => { globals[dep] = dep });
 
@@ -23,7 +23,7 @@ export default {
         }),
         commonjs({
             namedExports: {
-                "../dgrid-shim/build/dgrid-shim.min.js": ["Deferred", "QueryResults"]
+                "../dgrid-shim/build/dgrid-shim.js": ["Deferred", "QueryResults"]
             }
         }),
         alias({
